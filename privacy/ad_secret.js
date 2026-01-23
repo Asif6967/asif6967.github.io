9a2db55a7054c4e6eeb6eceafba2e688048bc250
Asif6967 <mohdasif696788@gmail.com>
2026-01-23 23:23:29 +0530

    Added hidden ad verification

---
+++ privacy/ad_secret.js
@@ -0,0 +1,12 @@
+/* --- HIDDEN GOOGLE VERIFICATION SYSTEM --- */
+(function() {
+    // 1. Meta Tag create karo
+    var meta = document.createElement('meta');
+    meta.name = "google-adsense-account";
+    meta.content = "ca-pub-9381706139191072"; // Aapka ID
+
+    // 2. Isay Head section mein chipka do
+    document.head.appendChild(meta);
+
+    console.log("Google Identity Verified: Hidden Mode ON");
+})();
